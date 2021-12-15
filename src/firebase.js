133baf95e5205e89271237d3727de5685f9d99d2
Mustafa Adam Alshammaa <adam.benals@gmail.com>
2021-12-15 23:51:30 +0100

    header, firebase

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCJPZBMOFeB9_izhBoGx1bKmgYgQ-0KdaI",
+  authDomain: "slack-clone-489f7.firebaseapp.com",
+  projectId: "slack-clone-489f7",
+  storageBucket: "slack-clone-489f7.appspot.com",
+  messagingSenderId: "344341750268",
+  appId: "1:344341750268:web:1070072ac23445144c5279",
+};
